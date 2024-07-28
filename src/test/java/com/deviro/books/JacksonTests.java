8039e9318303ae2f71ba10f6ef840d750d79342b
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-07-28 12:12:06 +0700

    Ignore Properties

--- src/test/java/com/deviro/books/JacksonTests.java
+++ src/test/java/com/deviro/books/JacksonTests.java
@@ -41,7 +41,7 @@ public class JacksonTests {
             .build();
 
     String json =
-        "{\\\"isbn\\\":\\\"978-0-13-478627-5\\\",\\\"title\\\":\\\"The Enigma of"
+        "{\"foo\":\"bar\", \"isbn\\\":\\\"978-0-13-478627-5\\\",\\\"title\\\":\\\"The Enigma of"
             + " Eternity\\\",\\\"author\\\":\\\"Aria Montgomery\",\"year\":\"2005\"}";
 
     final ObjectMapper objectMapper = new ObjectMapper();
